85484032b3bccc3da431028a094c31eb973713b0
DaengAe <164836227+DaengAe@users.noreply.github.com>
2024-04-08 19:31:58 +0900

    Add files via upload

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -640,4 +640,56 @@ setInterval(find_day, 1000*60*60*24);  //일 마다 디데이 기능 실행
 
 
 
+/*
+window.onload=function showPopup(hasFilter) {
+	const popup = document.querySelector('#popup-sec');
+  
+  if (hasFilter) {
+  	popup.classList.add('has-filter');
+  } else {
+  	popup.classList.remove('has-filter');
+  }
+  
+  popup.classList.remove('hide');
+}
 
+function closePopup() {
+	const popup = document.querySelector('#popup-sec');
+  popup.classList.add('hide');
+}
+*/
+
+
+
+
+    // 쿠키 생성
+	function setCookie( name, value, expiredays ) {  // 쿠키저장
+		var todayDate = new Date();  //date객체 생성 후 변수에 저장
+		todayDate.setDate( todayDate.getDate() + expiredays ); 
+			// 시간지정(현재시간 + 지정시간)
+		document.cookie = name + "=" + value + "; path=/; expires=" + todayDate.toUTCString() + ";"
+		//위 정보를 쿠키에 굽는다
+		} 
+		
+		
+		$(function(){
+		$(".popup_box").draggable({containment:'parent', scroll:false}); // 레이어 팝업 창 드래그 가능
+		//{containment:'parent', scroll:false} 화면 영역 밖으로 드래그 안됌.
+					
+		if(document.cookie.indexOf("popToday=close") < 0 ){      // 쿠키 저장여부 체크
+			document.getElementById("popup_layer").style.display = "block";
+			}else {
+			document.getElementById("popup_layer").style.display = "none"; 
+			}
+		});
+				 
+		//오늘하루만보기 닫기버튼 스크립트
+		function closeToday() { 
+		setCookie( "popToday", "close" , 1  ); 
+		$("#popup_layer").css("display", "none");
+		document.getElementById("popup_layer").style.display = "none";
+		}
+		//그냥 닫기버튼 스크립트
+		function closePop() { 
+		document.getElementById("popup_layer").style.display = "none";
+		}
