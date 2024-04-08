3878ea6bc77e415151ad31de2e0ac56a8adaff68
DaengAe <164836227+DaengAe@users.noreply.github.com>
2024-04-09 00:51:23 +0900

    Add files via upload

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -694,3 +694,12 @@ function closePopup() {
 		document.getElementById("popup_layer").style.display = "none";
 		}
 	*/
+
+
+	$(".que").click(function() {
+		$(this).next(".anw").stop().slideToggle(300);
+	   $(this).toggleClass('on').siblings().removeClass('on');
+	   $(this).next(".anw").siblings(".anw").slideUp(300); // 1개씩 펼치기
+	 });
+
+	
